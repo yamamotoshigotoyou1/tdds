1619f668399fd564ebc6c5950d80d6607556c77e
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-09-03 00:57:33 +0900

    [rust-unittest] Add test case tests

--- rust-unittest/src/xunit.rs
+++ rust-unittest/src/xunit.rs
@@ -9,15 +9,22 @@ struct WasRun {
 
 #[allow(dead_code)]
 impl WasRun {
-  pub fn new(name: &'static str) -> Self {
-    WasRun { was_run: 0, name }
-  }
-
   pub fn test_method(&mut self) {
     self.was_run = 1;
   }
+}
 
-  pub fn run(&mut self) {
+trait TestCase {
+  fn new(name: &'static str) -> Self;
+  fn run(&mut self);
+}
+
+impl TestCase for WasRun {
+  fn new(name: &'static str) -> Self {
+    WasRun { was_run: 0, name }
+  }
+
+  fn run(&mut self) {
     // TODO: alternative way for dynamic method invocation
     let mut methods: HashMap<&'static str, fn(&mut Self)> = HashMap::new();
     methods.insert("test_method", WasRun::test_method);
@@ -29,6 +36,29 @@ impl WasRun {
   }
 }
 
+struct TestCaseTest {
+  test: WasRun,
+}
+
+impl TestCaseTest {
+  pub fn test_running(&mut self) {
+    assert_eq!(0, self.test.was_run);
+    self.test.run();
+    assert_eq!(1, self.test.was_run);
+  }
+}
+
+impl TestCase for TestCaseTest {
+  fn new(name: &'static str) -> Self {
+    let test = WasRun { was_run: 0, name };
+    TestCaseTest { test }
+  }
+
+  fn run(&mut self) {
+    self.test_running();
+  }
+}
+
 #[cfg(test)]
 mod was_run_test {
   use super::*;
@@ -37,8 +67,14 @@ mod was_run_test {
   fn test_was_run() {
     let mut test = WasRun::new("test_method");
 
-    println!("{}", test.was_run);
+    assert_eq!(0, test.was_run);
     test.run();
-    println!("{}", test.was_run);
+    assert_eq!(1, test.was_run);
+  }
+
+  #[test]
+  fn test_case_running() {
+    let mut test_case_test = TestCaseTest::new("test_method");
+    test_case_test.run();
   }
 }
